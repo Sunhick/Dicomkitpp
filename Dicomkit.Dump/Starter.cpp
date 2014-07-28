10b6f0fa41e5d864039028b873ad39dccb6fb079
sandeep-kulkarni@users.noreply.github.com <sandy373894>
2014-07-28 23:04:20 +0530

    test branch for Vs integration

--- Dicomkit.Dump/Starter.cpp
+++ Dicomkit.Dump/Starter.cpp
@@ -13,6 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with DicomKit.  If not, see <http://www.gnu.org/licenses/>.
 
+//for sandeep branch , test branch
+
 #include <exception>
 
 #include "DicomDump.h"
