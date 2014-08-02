f9a7fd3edea1500e85adcb29f553bd69d7f6d54c
Sunil <sunhick@gmail.com>
2014-08-02 20:00:17 +0530

    updated file header from Dicomkit to Dicomkit++

--- Dicomkit.Sdk/DicomReader.h
+++ Dicomkit.Sdk/DicomReader.h
@@ -1,17 +1,17 @@
-// This file is part of Dicomkit.
+// This file is part of Dicomkit++.
 //  
-// DicomKit  is free software: you can redistribute it and/or modify
+// Dicomkit++  is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 // 
-// DicomKit is distributed in the hope that it will be useful,
+// Dicomkit++ is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 // 
 // You should have received a copy of the GNU General Public License
-// along with DicomKit.  If not, see <http://www.gnu.org/licenses/>.
+// along with Dicomkit++.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef DICOM_READER_H
 #define DICOM_READER_H
